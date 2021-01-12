fe93207a8322574f0742e8b1a5c405182a5eed97
danleks <danilevich.lks@gmail.com>
2021-01-12 22:27:06 +0100

    Create topic.js

---
+++ sanity/schemas/topic.js
@@ -0,0 +1,14 @@
+export default {
+    // Computer name
+    name: 'topic',
+    // visible title
+    title: 'Topics',
+    type: 'document',
+    fields: [
+        {
+            title: ' Add topic title',
+            name: 'topic',
+            type: 'string',
+        },
+    ]
+};
