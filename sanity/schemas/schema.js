32af90eb89b8a764164f3456de71a1f64578cd49
danleks <danilevich.lks@gmail.com>
2021-01-17 23:21:52 +0100

    Update schema.js

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -7,6 +7,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 // custom schemas
 import speaker from "./speaker";
 import topic from "./topic";
+import scheduleDetails from './scheduleDetails';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -18,5 +19,6 @@ export default createSchema({
     /* Your types here! */
       speaker,
       topic,
+      scheduleDetails,
   ]),
 })
