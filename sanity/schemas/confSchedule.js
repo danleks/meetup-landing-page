bdf5103e08e882a923e3780f101911d9076f3e5b
danleks <danilevich.lks@gmail.com>
2021-01-21 22:51:28 +0100

    Create confSchedule.js

---
+++ sanity/schemas/confSchedule.js
@@ -0,0 +1,52 @@
+const days = ['Day 1', 'Day 2', 'Day 3'];
+
+export default {
+    // Computer name
+    name: 'confSchedule',
+    // visible title
+    title: 'Conference schedule',
+    type: 'document',
+    fields: [
+        {
+            name: 'confDay',
+            title: 'Conference day',
+            type: 'string',
+            description: 'Select conference day',
+            options: {
+                list: days,
+            }
+        },
+        {
+            name: 'date',
+            title: 'Set the date',
+            type: 'date',
+        },
+        {
+            title: 'Speakers',
+            description: 'Add speakers to the current conference day - 4 speakers max.',
+            name: 'speakers',
+            type: 'array',
+            of: [{ type: 'scheduleDetails' }]
+        },
+    ],
+    preview: {
+        select: {
+            confDay: 'confDay',
+            date: 'date',
+            speaker0: 'speakers.0.currentSpeaker.name',
+            speaker1: 'speakers.1.currentSpeaker.name',
+            speaker2: 'speakers.2.currentSpeaker.name',
+            speaker3: 'speakers.3.currentSpeaker.name',
+
+        },
+        prepare({ confDay, date, ...speakers }) {
+            // Filter undefined speakers out
+            console.log(speakers);
+            const speakersFiltered = Object.values(speakers).filter(Boolean);
+            return {
+                title: `${confDay} - ${date}`,
+                subtitle: speakersFiltered.length > 0 ? speakersFiltered.join(', ') : 'No speakers set',
+            }
+        }
+    }
+}
