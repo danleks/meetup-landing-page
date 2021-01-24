addceab414da9b13546a49e093ae3afe7c1152c0
danleks <danilevich.lks@gmail.com>
2021-01-24 20:11:25 +0100

    Update index.js

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HeroTemplate from '../templates/hero';
 import DescriptionTemplate from "../templates/description";
 import SpeakersTemplate from "../templates/speakers";
 import TicketsTemplate from "../templates/tickets";
 import ScheduleTemplate from "../templates/schedule";
+import { graphql } from 'gatsby';
 
-const index = () => {
+const Index = ({ data, pageContext, location }) => {
+  
+  useEffect(() => {
+    if (location.state && location.state.position) {
+      window.scrollBy(0, location.state.position);
+    } 
+  })
+  const speakersData = data.speakers.nodes;
   return (
     <>
         <HeroTemplate />
         <DescriptionTemplate />
         <SpeakersTemplate />
         <TicketsTemplate />
-        <ScheduleTemplate />
+        <ScheduleTemplate speakersData={speakersData} />
     </>
   )
 }
 
-export default index;
+export const query = graphql`
+  query SpeakersQuery($scheduleRegex: String) {
+    speakers: allSanityConfSchedule(filter: {confDay: {regex: $scheduleRegex}}) {
+    nodes {
+      speakers {
+        currentSpeaker {
+          name
+          description
+          image {
+            asset {
+              fluid(maxWidth: 200) {
+                ...GatsbySanityImageFluid
+              }
+            }
+          }
+        }
+        speakerTopic {
+          topic
+        }
+        startSpeech
+        endSpeech
+      }
+    }
+  }
+  }
+`;
+
+export default Index;
